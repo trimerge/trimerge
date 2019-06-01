c1d6b20a39ecf3d3e659f26e891df67887cc49c3
Marcello Bastéa-Forte <marcello@descript.com>
2019-05-31 17:12:16 -0700

    more exports

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ export { JSONObject, JSONArray, JSONLiteral, JSONValue } from './json';
 export { default as jsonEqual } from './json-equal';
 export { Path, PathKey } from './path';
 export { CannotMerge } from './cannot-merge';
-export { combineMergers, trimergeEquality } from './trimerge';
+export {
+  combineMergers,
+  trimergeEquality,
+  trimergeEqualityCreator,
+  CannotMergeError,
+} from './trimerge';
 export {
   trimergeArrayCreator,
   trimergeJsonDeepEqual,
   trimergeJsonObject,
 } from './trimerge-json';
 export { routeMergers, RouteWildCard } from './trimerge-router';
+export { trimergeMap } from './trimerge-map';
+export { trimergeString } from './trimerge-string';
