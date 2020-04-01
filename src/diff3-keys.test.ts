e8f601787731f484807acf177bb59efe2f7490e1
Marcello Bastéa-Forte <marcello@descript.com>
2020-04-01 12:23:37 -0700

    restore diff3keys

--- src/diff3-keys.test.ts
+++ src/diff3-keys.test.ts
@@ -1,55 +1,34 @@
-import { trimergeOrderedMap } from './trimerge-ordered-map';
+import { diff3Keys } from './diff3-keys';
 
 describe('diff3Keys', () => {
   it('does nothing for no keys', () => {
     const callback = jest.fn();
-    trimergeOrderedMap([], [], [], callback);
+    diff3Keys([], [], [], callback);
     expect(callback.mock.calls).toEqual([]);
   });
   it('merges added keys', () => {
     const callback = jest.fn();
-    trimergeOrderedMap([], ['a'], ['b'], callback);
+    diff3Keys([], ['a'], ['b'], callback);
     expect(callback.mock.calls).toEqual([['a'], ['b']]);
   });
   it('merges removed keys', () => {
     const callback = jest.fn();
-    trimergeOrderedMap(['a', 'b', 'c'], ['a', 'b'], ['b', 'c'], callback);
+    diff3Keys(['a', 'b', 'c'], ['a', 'b'], ['b', 'c'], callback);
     expect(callback.mock.calls).toEqual([['b']]);
   });
   it('merges added and removed keys', () => {
     const callback = jest.fn();
-    trimergeOrderedMap(
-      ['a', 'b', 'c'],
-      ['a', 'b'],
-      ['a', 'b', 'c', 'd'],
-      callback,
-    );
+    diff3Keys(['a', 'b', 'c'], ['a', 'b'], ['a', 'b', 'c', 'd'], callback);
     expect(callback.mock.calls).toEqual([['a'], ['b'], ['d']]);
   });
   it('merges moved keys', () => {
     const callback = jest.fn();
-    trimergeOrderedMap(
-      ['a', 'b', 'c'],
-      ['c', 'a', 'b'],
-      ['b', 'a', 'c'],
-      callback,
-    );
+    diff3Keys(['a', 'b', 'c'], ['c', 'a', 'b'], ['b', 'a', 'c'], callback);
     expect(callback.mock.calls).toEqual([['c'], ['b'], ['a']]);
-  });
-  it('restores deleted key', () => {
-    const callback = jest.fn();
-    trimergeOrderedMap(
-      ['a', 'b', 'c'],
-      ['a', 'b'],
-      ['a', 'b', 'c'],
-      callback,
-      true,
-    );
-    expect(callback.mock.calls).toEqual([['a'], ['b'], ['c']]);
   });
   it('handles conflicting move when allowed', () => {
     const callback = jest.fn();
-    trimergeOrderedMap(
+    diff3Keys(
       ['a', 'b', 'c', 'd'],
       ['c', 'a', 'b', 'd'],
       ['a', 'b', 'd', 'c'],
@@ -60,7 +39,7 @@ describe('diff3Keys', () => {
   });
   it('handles conflicting move when allowed 2', () => {
     const callback = jest.fn();
-    trimergeOrderedMap(
+    diff3Keys(
       ['a', 'b', 'c', 'd'],
       ['a', 'b', 'd', 'c'],
       ['c', 'a', 'b', 'd'],
@@ -73,7 +52,7 @@ describe('diff3Keys', () => {
   it('throws on conflicting move', () => {
     const callback = jest.fn();
     expect(() =>
-      trimergeOrderedMap(
+      diff3Keys(
         ['a', 'b', 'c', 'd'],
         ['a', 'b', 'd', 'c'],
         ['c', 'a', 'b', 'd'],
